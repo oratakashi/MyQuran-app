2ec0f008a68423800823dbb54e56238283e41e66
Oratakashi Nhamako <oratakashi@oratakashi.com>
2022-08-11 23:27:34 +0700

    feature lottie detail json

--- app/src/main/java/com/oratakashi/myquran/presentation/menu/detail/DetailFragment.kt
+++ app/src/main/java/com/oratakashi/myquran/presentation/menu/detail/DetailFragment.kt
@@ -1,11 +1,5 @@
 package com.oratakashi.myquran.presentation.menu.detail
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.oratakashi.myquran.R
 import com.oratakashi.myquran.databinding.FragmentDetailBinding
 import com.oratakashi.myquran.presentation.abstaction.BaseFragment
 import com.oratakashi.viewbinding.core.binding.fragment.viewBinding
